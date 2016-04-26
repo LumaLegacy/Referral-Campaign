b1b2e84a59d6470ec005574dd06e37e58d48b021
Chris Cassano <glitch0@gmail.com>
2016-04-26 14:16:41 -0700

    Added control of ip_addresses

--- app/admin/ip_address.rb
+++ app/admin/ip_address.rb
@@ -1,12 +1,21 @@
 ActiveAdmin.register IpAddress do
-  csv do
+  index do
     column :id
     column :address
     column :count
     column :created_at
     column :updated_at
+    actions
   end
 
-  actions :index, :show
+  permit_params :count
 
+  # actions :index, :show
+
+  form do |f|
+    f.inputs "IP Details" do
+      f.input :count
+    end
+    f.actions
+  end
 end
--- app/admin/users.rb
+++ app/admin/users.rb
@@ -9,6 +9,16 @@ ActiveAdmin.register User do
     column :updated_at
   end
 
+  index do
+    column :id
+    column :email
+    column :referral_code
+    column :referrer_id
+    column :created_at
+    column :updated_at
+    actions
+  end
+
   actions :index, :show
 
 end
